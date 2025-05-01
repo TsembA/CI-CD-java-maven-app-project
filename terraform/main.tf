ad530d435175575594aeb47eec9408407a80d123
TsembA <tsembenhoi@gmail.com>
2025-04-30 23:29:28 -0700

    Initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,108 @@
+provider "aws" {
+  region = var.region
+}
+# Create VPC
+resource "aws_vpc" "myapp_vpc" {
+  cidr_block = var.vpc_cidr_block
+
+  tags = {
+    Name = "${var.env_prefix}-vpc"
+  }
+}
+
+# Create Subnet
+resource "aws_subnet" "myapp_subnet" {
+  vpc_id            = aws_vpc.myapp_vpc.id
+  cidr_block        = var.subnet_cidr_block
+  availability_zone = var.avail_zone
+
+  tags = {
+    Name = "${var.env_prefix}-subnet"
+  }
+}
+
+# Create Internet Gateway
+resource "aws_internet_gateway" "myapp_igw" {
+  vpc_id = aws_vpc.myapp_vpc.id
+
+  tags = {
+    Name = "${var.env_prefix}-igw"
+  }
+}
+
+# Associate subnet with default Route Table
+resource "aws_default_route_table" "main_rtb" {
+  default_route_table_id = aws_vpc.myapp_vpc.default_route_table_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp_igw.id
+  }
+
+  tags = {
+    Name = "${var.env_prefix}-main-rtb"
+  }
+}
+
+# Create Security Group
+resource "aws_default_security_group" "myapp_sg" {
+  vpc_id = aws_vpc.myapp_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
+    cidr_blocks = [var.my_ip, var.jenkins_ip]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.env_prefix}-default-sg"
+  }
+}
+
+# Fetch Latest Amazon Linux 2 AMI
+data "aws_ami" "latest-amazon-linux-image" {
+  most_recent = true
+  owners = ["amazon"]
+  filter {
+    name = "name" 
+    values = ["amzn2-ami-kernel-*-x86_64-gp2"]
+  }
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+# Create EC2 Instance
+resource "aws_instance" "myapp_server" {
+  ami                    = data.aws_ami.lts_amazon_linux.id
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.myapp_subnet.id
+  vpc_security_group_ids = [aws_default_security_group.myapp_sg.id]
+  availability_zone      = var.avail_zone
+  associate_public_ip_address = true
+  key_name               = "myapp-key"
+
+  tags = {
+    Name = "${var.env_prefix}-server"
+  }
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.myapp_server.public_ip
+}
